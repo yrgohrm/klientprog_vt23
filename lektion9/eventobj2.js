e10b077939777b10d9657d0260f58c823676b104
Hampus Ram <hampus.ram@educ.goteborg.se>
2023-03-22 10:31:20 +0100

    lektion 9

---
+++ lektion9/eventobj2.js
@@ -0,0 +1,37 @@
+const buttons = document.getElementsByTagName("body");
+for (const button of Array.from(buttons)) {
+    button.addEventListener('click', transform)
+}
+
+
+function transform(event) {
+    if (event.currentTarget === event.target) {
+        return
+    }
+
+    if (event.target.classList.contains("nice")) {
+        event.target.classList.remove("nice")
+    }
+    else {
+        event.target.classList.add("nice")
+    }
+}
+
+
+/* horrible pseudo code
+while (true) {
+    const nextEvent = getEventFromOS();
+    if (nextEvent === MOUSE_EVENT) {
+        const elem = getTargetFromOS();
+        let eventObject = new MouseEvent(elem, nextEvent.x, nextEvent.y);
+        elem.registeredClickHandler(eventObject)
+    }
+}
+
+
+addEventListener(elem, type, func) {
+    if (type === 'click') {
+        elem.registeredClickHandler = func
+    }
+}
+*/
