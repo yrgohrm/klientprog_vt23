a75622347eb6f7893b479d31f3c59e581812b204
Hampus Ram <hampus.ram@educ.goteborg.se>
2023-04-24 10:51:50 +0200

    lektion 15 + 16

---
+++ lektion15/vue-proj/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+    <div @click="changeMessage">
+        <CoolButton />{{ message }}
+    </div>
+</template>
+
+<script>
+import CoolButton from './CoolButton.vue';
+
+export default {
+    components: {
+        CoolButton,
+    },
+    data() {
+        return {
+            message: "Hello, world!"
+        }
+    },
+    methods: {
+        changeMessage() {
+            this.message = this.message.split("").reverse().join("")
+        }
+    },
+}
+</script>
+
+<style scoped>
+div {
+    background-color: hotpink;
+}
+</style>
