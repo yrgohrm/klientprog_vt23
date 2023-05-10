df92ae1f2779233dc74b226004257176f88af368
Hampus Ram <hampus.ram@educ.goteborg.se>
2023-05-10 11:34:17 +0200

    lektion 18

---
+++ lektion18/vite-vue-bootstrap-basics/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    Hello
+
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <a href="#" class="btn btn-secondary btn-sm">apa</a>
+      <a href="#" class="btn btn-primary btn-sm">bepa</a>
+      <a href="#" class="btn btn-warning btn-sm">cepa</a>
+    </div>
+
+    <div class="custom-element">apansson</div>
+    <div class="custom-element2">apansson</div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@import '~bootstrap/scss/mixins';
+@import '~bootstrap/scss/functions';
+@import '~bootstrap/scss/variables';
+
+.custom-element2 {
+  background-color: tint-color($primary, 10%);
+}
+</style>
---
+++ lektion18/vite-vue-bootstrap-basics/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.scss'
+import * as bootstrap from 'bootstrap'
+
+createApp(App).mount('#app')
